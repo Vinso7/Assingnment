de411c85f1b5b2b9c7ce662ebd1a7cab551c4604
Vinso7 <93050604+Vinso7@users.noreply.github.com>
2021-11-30 23:00:38 +0530

    Delete Spring Rest directory

--- Spring Rest/assignmentRest-8/src/main/java/com/practice/Repo/AdminRepo.java
+++
@@ -1,9 +0,0 @@
-package com.practice.Repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.practice.model.admin;
-
-public interface AdminRepo extends MongoRepository<admin,Integer> {
-
-}
